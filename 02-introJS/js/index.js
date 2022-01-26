603f27ceca04cc44584b06df505b4d1143a8b35a
sebsmg <94944100+sebsmg@users.noreply.github.com>
2022-01-25 18:20:04 -0600

    Add files via upload

---
+++ 02-introJS/js/index.js
@@ -0,0 +1,15 @@
+//Elementos para mostrar la salida de datos
+//
+console.log('Hola Mundo, desde un archivo externo')
+console.log(5+10)
+console.log(4==='4')
+
+//Muestra la salida en la pagina
+document.write('Hola Mundo, desde un archivo externo')
+document.write(5+10)
+document.write(4==='4')
+
+//Muestra la salida en caja
+alert('Hola Mundo, desde un archivo externo')
+alert(5+10)
+alert(4==='4')
